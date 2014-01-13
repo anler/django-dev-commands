6cde7ef36e16aac1452e5ff439eb9c510c262b1e
ikame <anler86@gmail.com>
2014-01-13 15:27:25 +0100

    Command `conf`

---
+++ django_dev_commands/management/commands/conf.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""Dump all django settings.
+
+If names are passed through the command line, limit the output to those that match.
+The match is case insensitive.
+"""
+import re
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+def settings():
+    from django.conf import settings
+
+    for name in dir(settings):
+        if not name.isupper():
+            continue
+        yield name, getattr(settings, name)
+
+
+def settings_limited_to(values):
+    regex = re.compile('|'.join(values), re.IGNORECASE)
+    for name, value in settings():
+        if regex.match(name):
+            yield name, value
+
+
+def list_settings(limit_to=None):
+    _settings = settings() if limit_to is None else settings_limited_to(limit_to)
+    for name, value in _settings:
+        yield name, value
+
+
+
+class Command(BaseCommand):
+    args = '<setting-name ...>'
+    help = __doc__
+
+    def handle(self, *args, **options):
+        for name, value in list_settings(limit_to=args):
+            self.stdout.write("{} = {!r}\n".format(name, value))
